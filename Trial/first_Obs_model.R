f07f0350d7bf078bc85e5a0c30def7f32ec5b60a
jms5151 <jamie.sziklay@gmail.com>
2014-10-21 12:12:31 -1000

    Update first_Obs_model.R

--- Trial/first_Obs_model.R
+++ Trial/first_Obs_model.R
@@ -1,14 +1,16 @@
 ## my first jags file ##
 
-model 
-{
-  theta ~ dbeta(1,1)
+model {
   
-  #alpha ~ unif(0,1)
-  #beta ~ unif(0,1)
-
   for (i in 1:N) {
-    #Krep[i] <- dbinom(Nc[i], theta)
-    K[i] ~ dbin(theta, Nc[i])    #syntax was wrong on this line.  dbin(p,N) not dbinom(N,p)
+  K[i] ~ dbin(theta, Nc[i])    #syntax was wrong on this line.  dbin(p,N) not dbinom(N,p)
+  theta <- ilogit(beta[0]+(beta[1]*X[i])+(beta[2]*Y[i]))
   }
+
+  beta ~ dnorm(mu, tau)
+  #mu ~ dnorm(0,1) should this be a prior? Should this be mu <- mean(X)?
+  tau <- 1 / pow(sigma, 2)
+  sigma ~ dunif(0,100)
+  #tau ~ dgamma(0.001, 0.001)
+  #sigma <- 1.0/sqrt(tau)
 }
